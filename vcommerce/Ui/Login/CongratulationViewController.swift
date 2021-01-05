a0fa483c911002c8ff191b8c460959140211d790
jeongupark <jeongu@getaim.co>
2021-01-05 17:28:29 +0900

    로그인관련 UI 작성 완료 (기능 작업 필요)

---
+++ vcommerce/Ui/Login/CongratulationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CongratulationViewController.swift
+//  vcommerce
+//
+//  Created by JeongU Park on 2021/01/05.
+//
+
+import Foundation
+import UIKit
+
+class CongratulationViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ vcommerce/Ui/Login/FindIdFromEmailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FindIdFromEmailViewController.swift
+//  vcommerce
+//
+//  Created by JeongU Park on 2021/01/05.
+//
+
+import Foundation
+import UIKit
+
+class FindIdFromEmailViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
